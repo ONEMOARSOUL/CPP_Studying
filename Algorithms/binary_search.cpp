75dcc258743cd4e415ba83a26260f33aae9d1391
Onemoarsoul <nevercoreme@gmail.com>
2024-01-29 14:47:25 +0300

    First time

---
+++ Algorithms/binary_search.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int binary_search (int arr[], int size, int item) {
+    int low = 0;
+    int high = size - 1;
+    
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int guess = arr[mid];
+        if (guess == item) {
+            return mid;
+        } else if (guess > item) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+        return -1;
+    }
+}
+int main () {
+
+int A[] = {1,3,5,7,9};
+int size = sizeof(A) / sizeof(A[0]);
+std::cout << binary_search (A, size, 5);
+
+}
